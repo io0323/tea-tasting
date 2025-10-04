fe8f80ebd488f85b3d4856065cdce5d2b6a12361
io0323 <iokuma0323@gmail.com>
2025-10-04 18:17:33 +0900

    ESLint設定ファイルと依存関係を追加
    - .eslintrc.jsファイルを作成（React Native/TypeScript用の設定）
    - package.jsonにESLint関連の依存関係を追加
    - lintとlint:fixスクリプトを追加
    - GitHub ActionsのESLintエラーを解決

---
+++ .eslintrc.js
@@ -0,0 +1,73 @@
+/**
+ * ESLint設定ファイル
+ * React Native/Expoプロジェクト用の設定
+ */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    // 基本的なルール
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'max-len': ['error', { code: 80 }],
+    
+    // React関連のルール
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    
+    // TypeScript関連のルール
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    
+    // コメント関連のルール
+    'require-jsdoc': ['error', {
+      require: {
+        FunctionDeclaration: true,
+        MethodDefinition: true,
+        ClassDeclaration: true,
+        ArrowFunctionExpression: true,
+        FunctionExpression: true,
+      },
+    }],
+    
+    // その他のルール
+    'no-console': 'warn',
+    'no-debugger': 'error',
+    'prefer-const': 'error',
+    'no-var': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  ignorePatterns: [
+    'node_modules/',
+    'dist/',
+    'build/',
+    '.expo/',
+    'web-build/',
+  ],
+};
